12cc1964550eed781df3eb094c38f01f06a384ad
Marcelo Barboza <60588536+Marcelo7211@users.noreply.github.com>
2020-07-09 10:34:58 -0300

    Primeiro commit
    Primeiro projeto em spring Hello World

---
+++ hello/src/test/java/com/halloworld/hallo/HalloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.halloworld.hallo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
